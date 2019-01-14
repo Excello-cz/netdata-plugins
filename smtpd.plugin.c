1f0693c2ef1cf3a9a814dd4e59a6758eecabfaba
Petr Vaněk <arkamar@atlas.cz>
2019-01-14 21:47:50 +0100

    smtpd: parse arguments

--- smtpd.plugin.c
+++ smtpd.plugin.c
@@ -3,10 +3,21 @@
 
 int
 main(int argc, char * argv[]) {
+	const char * file_name;
+	const char * argv0;
 	int update = 1;
 
-	if (argc > 1)
-		update = atoi(argv[1]);
+	argv0 = *argv; argv++; argc--;
+
+	if (argc >= 1) {
+		update = atoi(*argv);
+		argv++; argc--;
+	}
+
+	if (argc) {
+		file_name = *argv;
+		argv++; argc--;
+	}
 
 	return 0;
 }
