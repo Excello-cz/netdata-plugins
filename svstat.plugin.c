a40191e2d681f6c403a15fc3f6f98943c2468e8e
Petr Vaněk <pv@excello.cz>
2019-08-30 10:25:44 +0200

    svstat: introduce svstat plugin
    The plugin collects uptime/downtime information for deamontools
    services.

---
+++ svstat.plugin.c
@@ -0,0 +1,140 @@
+/* SPDX-License-Identifier: GPL-3.0-or-later */
+
+#include <dirent.h>
+#include <endian.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+
+#include "err.h"
+#include "netdata.h"
+
+#include "fs.h"
+
+#define DEFAULT_PATH "/service"
+
+struct dt_stat {
+	uint64_t seconds;
+	uint32_t nano;
+	uint32_t pid;
+	uint8_t  paused;
+	uint8_t  want;
+};
+
+static
+void
+usage(const char * name) {
+	fprintf(stderr, "usage: %s <timeout> [path]\n", name);
+}
+
+static inline
+uint64_t
+tai_now() {
+	return 4611686018427387914ULL + time(NULL);
+}
+
+uint64_t
+collect_uptime(const char * dir) {
+	unsigned char status[18]; /* See daemontools code */
+	struct dt_stat * stat;
+	uint64_t time;
+	int fd, ret;
+
+	if (chdir(dir) == -1) {
+		fprintf(stderr, "Cannot change directory to '%s': %s\n", dir, strerror(errno));
+		return 0;
+	}
+
+	fd = open("supervise/status", O_RDONLY | O_NDELAY);
+	if (fd == -1) {
+		fprintf(stderr, "Cannot open supervise/status: %s\n", strerror(errno));
+		return 0;
+	}
+
+	ret = read(fd, status, sizeof status);
+	if (ret < sizeof status) {
+		fprintf(stderr, "Cannot read supervise/status\n");
+		return 0;
+	}
+	close(fd);
+
+	stat = (void *)status;
+	time = tai_now() - be64toh(stat->seconds);
+	if (!stat->pid)
+		time *= -1;
+
+	return time;
+}
+
+int
+main(int argc, char * argv[]) {
+	struct dirent * dir_entry;
+	const char * dir_name;
+	const char * argv0;
+	const char * path;
+	int timeout = 1;
+	DIR * dir;
+
+	path = DEFAULT_PATH;
+	argv0 = *argv; argv++; argc--;
+
+	if (argc > 0) {
+		timeout = atoi(*argv);
+		argv++; argc--;
+	} else {
+		usage(argv0);
+	}
+
+	if (argc > 0) {
+		path = *argv;
+		argv++; argc--;
+	}
+
+	if (chdir(path) == -1) {
+		fprintf(stderr, "Cannot change directory to '%s': %s\n", path, strerror(errno));
+		exit(1);
+	}
+
+	dir = opendir(".");
+	nd_chart("daemontools", "svstat", NULL, NULL, NULL, "time", NULL, "daemontools.svstat", ND_CHART_TYPE_LINE);
+	while ((dir_entry = readdir(dir))) {
+		dir_name = dir_entry->d_name;
+
+		if (dir_name[0] == '.')
+			continue;
+
+		if (is_directory(dir_name) == 1)
+			nd_dimension(dir_name, dir_name, ND_ALG_ABSOLUTE, 1, 1, ND_VISIBLE);
+	}
+	fflush(stdout);
+
+	while (1) {
+		rewinddir(dir);
+		nd_begin("daemontools", "svstat", NULL);
+		while ((dir_entry = readdir(dir))) {
+			dir_name = dir_entry->d_name;
+
+			if (dir_name[0] == '.')
+				continue;
+
+			if (is_directory(dir_name) == 1) {
+				nd_set(dir_name, collect_uptime(dir_name));
+			}
+
+			if (fchdir(dirfd(dir)) == -1) {
+				fprintf(stderr, "Cannot change directory back to '%s': %s\n", path, strerror(errno));
+				break;
+			}
+		}
+		nd_end();
+		fflush(stdout);
+		sleep(timeout);
+	}
+	closedir(dir);
+	return 0;
+}
