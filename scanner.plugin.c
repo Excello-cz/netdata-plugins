Excello-cz/netdata-plugins

commit 5ddc580e230d3cd7d5dc4c9157c4e7e5567273d9

scanner: extend scanner plugin for monitoring of the current log

scanner.plugin.c
@@ -88,7 +88,12 @@ detect_log_dirs(const int fd, struct vector * v) {
 				watch.file_name = "details";
 				watch.dir_name = strdup(dir_name);
 
-				if (prepare_watcher(&watch, fd, scanner_func) == ND_SUCCESS)
+				if (prepare_watcher(&watch, fd, details_func) == ND_SUCCESS)
+					vector_add(v, &watch);
+
+				watch.file_name = "current";
+
+				if (prepare_watcher(&watch, fd, scannerd_func) == ND_SUCCESS)
 					vector_add(v, &watch);
 			}
 		}
@@ -154,7 +159,7 @@ main(int argc, const char * argv[]) {
 
 	for (i = 0; i < vector.len; i++) {
 		watch = vector_item(&vector, i);
-		watch->func->print_hdr(watch->dir_name);
+		watch->func->print_hdr(watch->file_name);
 		clock_gettime(CLOCK_REALTIME, &watch->time);
 	}
 
@@ -186,7 +191,7 @@ main(int argc, const char * argv[]) {
 						watch->func->postprocess(watch->data);
 
 					last_update = update_timestamp(&watch->time);
-					if (watch->func->print(watch->dir_name, watch->data, last_update)) {
+					if (watch->func->print(watch->file_name, watch->data, last_update)) {
 						run = 0;
 						fprintf(stderr, "Cannot write to stdout: %s\n", strerror(errno));
 						break;

scanner.h
@@ -1,3 +1,4 @@
 /* SPDX-License-Identifier: GPL-3.0-or-later */
 
-extern struct stat_func * scanner_func;
+extern struct stat_func * details_func;
+extern struct stat_func * scannerd_func;

commit c00c848b532da86364ef034ef27d75aad6b15e46

scanner: fix double free error
watch.dir_name must contain unique address of duplicated dir_name,
otherwise double-free error can segfault the program, when resources are
released before the exit.

Fixes: 5ddc580e230d ("scanner: extend scanner plugin for monitoring of the current log")

scanner.plugin.c
@@ -92,6 +92,7 @@ detect_log_dirs(const int fd, struct vector * v) {
 					vector_add(v, &watch);
 
 				watch.file_name = "current";
+				watch.dir_name = strdup(dir_name);
 
 				if (prepare_watcher(&watch, fd, scannerd_func) == ND_SUCCESS)
 					vector_add(v, &watch);